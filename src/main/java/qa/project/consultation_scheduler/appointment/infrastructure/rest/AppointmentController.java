47f44f804db1b156e6451cbb2b83368db5e92743
FauricioTec <fagranados@estudiantec.cr>
2024-09-07 23:33:25 -0600

    Se habilitaron los CORS

--- src/main/java/qa/project/consultation_scheduler/appointment/infrastructure/rest/AppointmentController.java
+++ src/main/java/qa/project/consultation_scheduler/appointment/infrastructure/rest/AppointmentController.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/appointments")
 @AllArgsConstructor
